b2833d8600de49367b80cb6b46315bbabf3b8106
Anmol Raj Shakya <shakyaanmol33@gmail.com>
2025-09-19 18:20:50 +0545

    add a ball sprite and give it a circle shape

--- code/sprites.py
+++ code/sprites.py
@@ -1,4 +1,5 @@
 from settings import *
+from random import choice, uniform
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, groups):
@@ -25,3 +26,42 @@ class Player(pygame.sprite.Sprite):
     def update(self, dt):
         self.get_direction()
         self.move(dt)
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, groups, paddle_sprites):
+        super().__init__(groups)
+
+        #image
+        self.image = pygame.Surface(SIZE['ball'], pygame.SRCALPHA)
+        pygame.draw.circle(self.image,COLORS['ball'], (SIZE['ball'][0] / 2, SIZE['ball'][1] / 2),SIZE['ball'][0] / 2 )
+        #self.image.fill(COLORS['ball'])
+
+        #rect $ Movement
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.direction = pygame.Vector2(choice((1,-1)), uniform(0.7, 0.8) * choice((-1,1)))
+
+    def move(self, dt):
+        self.rect.center += self.direction * SPEED['ball'] * dt
+    
+    def wall_collision(self):
+        if self.rect.top <= 0:
+            self.rect.top = 0
+            self.direction.y *= -1
+
+        if self.rect.bottom >= WINDOW_HEIGHT:
+            self.rect.bottom = WINDOW_HEIGHT
+            self.direction.y *= -1
+
+        if self.rect.right >= WINDOW_WIDTH:
+            self.rect.right = WINDOW_WIDTH
+            self.direction.x *= -1
+        
+        if self.rect.left <= 0:
+            self.rect.left = 0
+            self.direction.x *= -1
+
+
+    def update(self, dt):
+      self.move(dt)
+      self.wall_collision()
